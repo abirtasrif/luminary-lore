391c0cc6ae5c26880ac523053712e4ab1c8d40ae
abirtasrif <abirtasrif@gmail.com>
2023-06-21 01:40:46 +0600

    added single blog test

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,40 @@
+import fs from "fs";
+import path from "path";
 import Head from "next/head";
+import matter from "gray-matter";
 
-export default function Home() {
+export default function Home(posts) {
   return (
-    <main>
+    <div>
       <Head>
         <title>Abir&apos;s Blog</title>
       </Head>
 
       <h2>Welcome</h2>
-    </main>
+    </div>
   );
+}
+
+export async function getStaticProps() {
+  const files = fs.readdirSync(path.join("posts"));
+
+  const posts = files.map((filename) => {
+    const slug = filename.replace(".md", "");
+
+    const mdMeta = fs.readFileSync(path.join("posts", filename), "utf-8");
+
+    const { data: frontmatter } = matter(mdMeta);
+
+    return {
+      slug,
+      frontmatter,
+    };
+  });
+
+  console.log(posts);
+  return {
+    props: {
+      posts: "The Posts",
+    },
+  };
 }
---
+++ posts/test.md
@@ -0,0 +1,13 @@
+---
+title: "Test Post"
+date: "June 24 2021"
+excerpt: "This is the excert"
+cover_image: "/public/images/posts/img1.png"
+---
+
+## Heading Two
+
+- Item 1
+- Item 2
+
+Hello world
